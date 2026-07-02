2b31087a181c9e8926283931da7efed761b1f105
kurapati.manisha <kurapatimanisha7@gmail.com>
2026-07-03 00:40:02 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import Weather from './components/Weather'
+import EnvTest from './components/EnvTest'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <EnvTest />
+      <Weather />
+    </div>
+  )
+}
+
+export default App
